62d40c0beae5e8a6ccadfeaae30a922e6d2e2d52
Lzduque <lzduque@hotmail.com>
2019-05-16 21:54:50 +0000

    authetification for admin and .env variables

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,7 +3,7 @@ class OrderMailer < ApplicationMailer
 
   def order_email(order)
     @order = order
-    @url  = `http://localhost:3000/orders/#{@order.id}`
-    mail(to: @order.email, subject: `#{@order.id}`)
+    @url  = "http://localhost:3000/orders/#{@order.id}"
+    mail(to: @order.email, subject: "Order: #{@order.id}")
   end
 end
