5e3fc9c63a0361a21ea65ff3a6b4c5560c18f9fa
Lzduque <lzduque@hotmail.com>
2019-05-17 13:23:16 +0000

    validation of rating model

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,2 +1,9 @@
 class Rating < ActiveRecord::Base
+
+  has_many :products
+  belongs_to :users
+
+  validates :products, presence: true
+  validates :users, presence: true
+  validates :ratings, presence: true, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
