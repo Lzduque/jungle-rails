f731b804715983404e840eb4ea518bdbc970a12a
Lzduque <lzduque@hotmail.com>
2019-05-18 20:20:30 +0000

    minor adjusts - user cannot signup with email that is already in database

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,4 +1,7 @@
 class AdminController < ApplicationController
+  # http_basic_authenticate_with name: ENV['ADMIN_USER'].to_s, password: ENV['ADMIN_PASSWORD'].to_s
+  # Bonus: Should ideally be implemented in an abstract top-level class for admin controllers (e.g., Admin::BaseController which the other concrete admin controllers inherit)
+
   def categories
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,11 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+
+    if User.find_by(email: user.email)
+      redirect_to('/signup') and return
+    end
+
     if user.save
       session[:user_id] = user.id
       redirect_to '/'
